a904cfe73d42ac8287a111388098824f65494913
Jonathan Hemi <jonathan.hemi@ef.com>
2020-08-25 13:31:23 +0800

    Initial Commit

---
+++ Shared/aaaaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaaaaApp.swift
+//  Shared
+//
+//  Created by jonathan on Aug/25/20.
+//
+
+import SwiftUI
+
+@main
+struct aaaaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
